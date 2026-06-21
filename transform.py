51f00d559274ac3f637425e513149e15a60bed31
Juan Aguinaga <juan_a03@icloud.com>
2026-06-21 01:08:41 -0500

    removed the column vaccine_product

--- transform.py
+++ transform.py
@@ -12,7 +12,7 @@ def get_raw_data():
 # print(df['age_group'].unique())
 
 def clean_data(df):
-   df = df.drop(columns=['_id', 'mmwr_week'])
+   df = df.drop(columns=['_id', 'mmwr_week', 'vaccine_product'])
    df = df[df.age_group != 'all_ages_adj']
    df = df.drop(columns=['age_adj_vax_ir', 'age_adj_unvax_ir', 'age_adj_irr'])
    float_col = ['vaccinated_with_outcome', 'fully_vaccinated_population', 'unvaccinated_with_outcome', 'unvaccinated_population', 'crude_vax_ir', 'crude_unvax_ir', 'continuity_correction', 'crude_irr']
