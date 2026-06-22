ecef3522fb51432fbcf9b70b59b17a94d6796a88
Juan Aguinaga <juan_a03@icloud.com>
2026-06-22 00:12:49 -0500

    trying to get airflow to work

--- extract.py
+++ extract.py
@@ -2,9 +2,10 @@ import requests
 from pymongo import MongoClient
 
 def extract_data():
-   url = 'https://data.cdc.gov/resource/3rge-nu2a.json?$limit=2000'
-   response = requests.get(url)
-   return response.json()
+    url = 'https://data.cdc.gov/resource/3rge-nu2a.json?$limit=2000'
+    headers = {'Accept': 'application/json'}
+    response = requests.get(url, timeout=30, headers=headers)
+    return response.json()
 
 def store_raw_data(data):
    client = MongoClient('localhost', 27017)
